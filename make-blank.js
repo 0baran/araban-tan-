a5700b0f8c4cd629c5759b6fa078506077ce57a1
0baran <0baran@users.noreply.github.com>
2026-05-21 16:39:23 +0300

    feat: user custom sari motor ariza logo eklendi

---
+++ make-blank.js
@@ -0,0 +1,6 @@
+const Jimp = require('jimp');
+new Jimp(1024, 1024, '#3498db', async (err, image) => {
+  if (err) throw err;
+  await image.writeAsync('blank.png');
+  console.log('Created blank.png');
+});
