6eff777292b48e998fada960c2c0e2a6560f8d5a
0baran <0baran@users.noreply.github.com>
2026-05-22 10:07:05 +0300

    v2.9.11: Bump version and update changelog

--- App.tsx
+++ App.tsx
@@ -26,7 +26,7 @@ import VehiclesScreen from './src/screens/VehiclesScreen';
 import {ThemeProvider, useTheme} from './src/services/ThemeContext';
 import {checkForUpdate, promptUpdate} from './src/services/UpdateService';
 
-const APP_VERSION = '2.9.10';
+const APP_VERSION = '2.9.11';
 
 export default function App() {
   return (
